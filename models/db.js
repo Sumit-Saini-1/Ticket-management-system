6fc3f5ec1a7482e47671f7e6a382ee49d45d77e2
Sumit-Saini-1 <113706814+Sumit-Saini-1@users.noreply.github.com>
2023-12-29 09:50:18 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
 
 module.exports.init=async function(){
-    await mongoose.connect("mongodb+srv://admin:k9k95NUyU4xiit6M@cluster0.h1usxsj.mongodb.net/ticketmanagement?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://admin:@cluster0.h1usxsj.mongodb.net/ticketmanagement?retryWrites=true&w=majority");
 }
